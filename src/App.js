235ea09e9909fa0b66e66dcc1046eef0c9cea1ea
caogiahuy13 <caogiahuy13@gmail.com>
2020-06-07 20:36:55 +0900

    Setup components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+
+import { Card, Chart, CountryPicker } from "./components/index";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className={styles.container}>
+      <Card></Card>
+      <Chart></Chart>
+      <CountryPicker />
     </div>
   );
 }
