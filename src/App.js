536bbbe28c30857f64bdb740a2b0995fd8a40d79
caogiahuy13 <caogiahuy13@gmail.com>
2020-06-07 22:12:37 +0900

    Setup API call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
-
+import React, { Component } from "react";
 import { Card, Chart, CountryPicker } from "./components/index";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <Card></Card>
-      <Chart></Chart>
-      <CountryPicker />
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+    console.log(fetchedData);
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Card></Card>
+        <Chart></Chart>
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
 export default App;
